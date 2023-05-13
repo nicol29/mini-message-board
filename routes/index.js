ae686b005f16178ead2befc44472496d6c9a1f7c
nicol29 <nicolascondrea@gmail.com>
2023-05-13 12:52:34 +0100

    Create simple dashboard message app using express

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+
+/* GET users page. */
+router.get("/new", function (req, res, next) {
+  res.render('new');
+});
+
+/* Post users page */
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message, 
+    user: req.body.name, 
+    added: new Date()
+  });
+  
+  res.redirect('/');
+});
+
+
+module.exports = router;
